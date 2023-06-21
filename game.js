bf89b566edf0e65c26cf01edfd66baaebb36bd3b
Leo B <leonardo_picinin02@hotmail.com>
2023-06-21 00:53:13 -0300

    mapa, player e inventario

--- game.js
+++ game.js
@@ -5,20 +5,116 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 window.onload = function () {
   const screenWidth = 800;
   const screenHeight = 600;
+  let choosedPlayer = "";
 
   const game = new Game(screenWidth, screenHeight);
   game.preload([
     "./images/characters/dargan.png",
     "./images/characters/elda.png",
     "./images/characters/zyra.png",
+    "./images/sprites/default.png",
+    "./images/sprites/dargan.png",
+    "./images/sprites/elda.png",
+    "./images/sprites/zyra.png",
+    "./images/items/common_sword.png",
+    "./images/items/green_sword.png",
+    "./images/items/poison_dagger.png",
+    "./images/items/warlord_sword.png",
   ]); // Preload de recursos, como imagens
 
-  const menuScene = () => {
-    const menu = new Group(); // Grupo para conter as opções de personagens
-    menu.x = 140; // Posição horizontal do menu
-    menu.y = 150; // Posição vertical do menu
-    menu.scaleX = 0.5;
-    menu.scaleY = 0.5;
+  const menuScene = new Scene();
+  const mainScene = new Scene();
+  const inventoryScene = new Scene();
+
+  const createMap = () => {
+    const spriteWidth = 16;
+    const spriteHeight = 16;
+
+    const map = new Map(spriteWidth, spriteHeight);
+    map.image = game.assets["./images/sprites/default.png"];
+    map.loadData(mapData);
+
+    const foregroundMap = new Map(spriteWidth, spriteHeight);
+    foregroundMap.image = game.assets["./images/sprites/default.png"];
+    foregroundMap.loadData(foregroundData);
+
+    const collisionData = [];
+    for (var i = 0; i < foregroundData.length; i++) {
+      collisionData.push([]);
+      for (var j = 0; j < foregroundData[0].length; j++) {
+        var collision = foregroundData[i][j] % 13 > 1 ? 1 : 0;
+        collisionData[i][j] = collision;
+      }
+    }
+
+    map.collisionData = collisionData;
+    return { map, foregroundMap };
+  };
+
+  const createPlayer = () => {
+    const spriteWidth = 16;
+    const spriteHeight = 16;
+
+    const player = new Sprite(spriteWidth, spriteHeight);
+    player.image = game.assets[choosedPlayer];
+    player.frame = 6;
+    player.animationSpeed = 0.2;
+    player.addEventListener("enterframe", () => {
+      if (game.input.up) {
+        this.frame = (Math.floor(this.age * this.animationSpeed) % 2) + 8; // frames 8 e 9 para cima
+      } else if (game.input.down) {
+        this.frame = (Math.floor(this.age * this.animationSpeed) % 2) + 10; // frames 10 e 11 para baixo
+      } else if (game.input.left) {
+        this.frame = (Math.floor(this.age * this.animationSpeed) % 2) + 2; // frames 2 e 3 para esquerda
+      } else if (game.input.right) {
+        this.frame = (Math.floor(this.age * this.animationSpeed) % 2) + 4; // frames 4 e 5 para direita
+      } else {
+        // Se nenhuma tecla está pressionada, usa o frame 0 (parado)
+        this.frame = 0;
+      }
+    });
+
+    player.x = 152; // Posição inicial do personagem
+    player.y = 152;
+
+    return player;
+  };
+
+  const createMainScene = () => {
+    const { map, foregroundMap } = createMap();
+    const player = createPlayer();
+
+    const mainSceneGroup = new Group();
+    mainSceneGroup.addChild(map);
+    mainSceneGroup.addChild(player);
+    mainSceneGroup.addChild(foregroundMap);
+    mainSceneGroup.scaleX = 2;
+    mainSceneGroup.scaleY = 2;
+
+    mainScene.addEventListener("enterframe", () => {
+      if (game.input.up) {
+        player.y -= 2;
+      }
+      if (game.input.down) {
+        player.y += 2;
+      }
+      if (game.input.left) {
+        player.x -= 2;
+      }
+      if (game.input.right) {
+        player.x += 2;
+      }
+    });
+
+    mainScene.addChild(mainSceneGroup);
+  };
+
+  const createMenuScene = () => {
+    const menuSceneGroup = new Group();
+    menuSceneGroup.x = 140; // Posição horizontal do menu
+    menuSceneGroup.y = 150; // Posição vertical do menu
+    menuSceneGroup.scaleX = 0.5;
+    menuSceneGroup.scaleY = 0.5;
 
     const startX = 0;
     const imageSize = 512;
@@ -40,6 +136,10 @@ window.onload = function () {
           await sleep(50);
           option1.opacity = option1.opacity + 0.1;
         }
+
+        choosedPlayer = "./images/sprites/dargan.png";
+        createMainScene();
+        game.replaceScene(mainScene);
       }
 
       alteraOpacity();
@@ -51,8 +151,8 @@ window.onload = function () {
     label1.font = "30px Arial";
     label1.color = "#FFF";
 
-    menu.addChild(option1);
-    menu.addChild(label1);
+    menuSceneGroup.addChild(option1);
+    menuSceneGroup.addChild(label1);
 
     // Opção de Personagem 2 (semelhante às opções restantes)
     const option2 = new Sprite(imageSize, imageSize);
@@ -69,6 +169,10 @@ window.onload = function () {
           await sleep(50);
           option2.opacity = option2.opacity + 0.1;
         }
+
+        choosedPlayer = "./images/sprites/elda.png";
+        createMainScene();
+        game.replaceScene(mainScene);
       }
 
       alteraOpacity();
@@ -80,8 +184,8 @@ window.onload = function () {
     label2.font = "30px Arial";
     label2.color = "#FFF";
 
-    menu.addChild(option2);
-    menu.addChild(label2);
+    menuSceneGroup.addChild(option2);
+    menuSceneGroup.addChild(label2);
 
     // Opção de Personagem 3 (semelhante às opções restantes)
     const option3 = new Sprite(imageSize, imageSize);
@@ -98,6 +202,10 @@ window.onload = function () {
           await sleep(50);
           option3.opacity = option3.opacity + 0.1;
         }
+
+        choosedPlayer = "./images/sprites/zyra.png";
+        createMainScene();
+        game.replaceScene(mainScene);
       }
 
       alteraOpacity();
@@ -109,14 +217,202 @@ window.onload = function () {
     label3.font = "30px Arial";
     label3.color = "#FFF";
 
-    menu.addChild(option3);
-    menu.addChild(label3);
+    menuSceneGroup.addChild(option3);
+    menuSceneGroup.addChild(label3);
 
-    game.rootScene.addChild(menu);
+    menuScene.addChild(menuSceneGroup);
+    game.replaceScene(menuScene);
+  };
+
+  const inventarioScene = () => {
+    const inventario = new Group(); // Grupo para conter as opções de personagens
+    inventario.x = 140; // Posição horizontal do menu
+    inventario.y = 150; // Posição vertical do menu
+
+    const startX = -60;
+
+    const commonSword = new Sprite(320, 320);
+    commonSword.image = game.assets["./images/items/common_sword.png"];
+    commonSword.scaleX = 0.2; // Redimensiona pela metade na horizontal
+    commonSword.scaleY = 0.2; // Redimensiona pela metade na vertical
+    commonSword.x = startX;
+    commonSword.y = -160;
+    commonSword.addEventListener("touchstart", () => {
+      commonSword.opacity = 0.3;
+      commonSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          commonSword.opacity = commonSword.opacity + 0.1;
+          commonSwordLabel.opacity = commonSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const commonSwordLabel = new Label("Common Sword(atk: 1)");
+    commonSwordLabel.x = 160;
+    commonSwordLabel.y = -15;
+    commonSwordLabel.font = "29px Arial";
+    commonSwordLabel.color = "#FFF";
+    commonSwordLabel.addEventListener("touchstart", () => {
+      commonSword.opacity = 0.3;
+      commonSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          commonSword.opacity = commonSword.opacity + 0.1;
+          commonSwordLabel.opacity = commonSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const greenSword = new Sprite(320, 320);
+    greenSword.image = game.assets["./images/items/green_sword.png"];
+    greenSword.scaleX = 0.2; // Redimensiona pela metade na horizontal
+    greenSword.scaleY = 0.2; // Redimensiona pela metade na vertical
+    greenSword.x = startX;
+    greenSword.y = -70;
+    greenSword.addEventListener("touchstart", () => {
+      greenSword.opacity = 0.3;
+      greenSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          greenSword.opacity = greenSword.opacity + 0.1;
+          greenSwordLabel.opacity = greenSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const greenSwordLabel = new Label("Green Sword(atk: 3)");
+    greenSwordLabel.x = 160;
+    greenSwordLabel.y = 75;
+    greenSwordLabel.font = "29px Arial";
+    greenSwordLabel.color = "#FFF";
+    greenSwordLabel.addEventListener("touchstart", () => {
+      greenSword.opacity = 0.3;
+      greenSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          greenSword.opacity = greenSword.opacity + 0.1;
+          greenSwordLabel.opacity = greenSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const poisonDagger = new Sprite(320, 320);
+    poisonDagger.image = game.assets["./images/items/poison_dagger.png"];
+    poisonDagger.scaleX = 0.2; // Redimensiona pela metade na horizontal
+    poisonDagger.scaleY = 0.2; // Redimensiona pela metade na vertical
+    poisonDagger.x = startX;
+    poisonDagger.y = 20;
+    poisonDagger.addEventListener("touchstart", () => {
+      poisonDagger.opacity = 0.3;
+      poisonDaggerLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          poisonDagger.opacity = poisonDagger.opacity + 0.1;
+          poisonDaggerLabel.opacity = poisonDaggerLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const poisonDaggerLabel = new Label("Poison Dagger(atk: 7)");
+    poisonDaggerLabel.x = 160;
+    poisonDaggerLabel.y = 165;
+    poisonDaggerLabel.font = "29px Arial";
+    poisonDaggerLabel.color = "#FFF";
+    poisonDaggerLabel.addEventListener("touchstart", () => {
+      poisonDagger.opacity = 0.3;
+      poisonDaggerLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          poisonDagger.opacity = poisonDagger.opacity + 0.1;
+          poisonDaggerLabel.opacity = poisonDaggerLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const warlodSword = new Sprite(320, 320);
+    warlodSword.image = game.assets["./images/items/warlord_sword.png"];
+    warlodSword.scaleX = 0.2; // Redimensiona pela metade na horizontal
+    warlodSword.scaleY = 0.2; // Redimensiona pela metade na vertical
+    warlodSword.x = startX;
+    warlodSword.y = 110;
+    warlodSword.addEventListener("touchstart", () => {
+      warlodSword.opacity = 0.3;
+      wardlordSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          warlodSword.opacity = warlodSword.opacity + 0.1;
+          wardlordSwordLabel.opacity = wardlordSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    const wardlordSwordLabel = new Label("Warlord Sword(atk: 15)");
+    wardlordSwordLabel.x = 160;
+    wardlordSwordLabel.y = 255;
+    wardlordSwordLabel.font = "29px Arial";
+    wardlordSwordLabel.color = "#FFF";
+    wardlordSwordLabel.addEventListener("touchstart", () => {
+      warlodSword.opacity = 0.3;
+      wardlordSwordLabel.opacity = 0.3;
+
+      async function alteraOpacity() {
+        for (let i = 0; i < 7; i++) {
+          await sleep(50);
+          warlodSword.opacity = warlodSword.opacity + 0.1;
+          wardlordSwordLabel.opacity = wardlordSwordLabel.opacity + 0.1;
+        }
+      }
+
+      alteraOpacity();
+    });
+
+    inventario.addChild(commonSword);
+    inventario.addChild(commonSwordLabel);
+
+    inventario.addChild(greenSword);
+    inventario.addChild(greenSwordLabel);
+
+    inventario.addChild(poisonDagger);
+    inventario.addChild(poisonDaggerLabel);
+
+    inventario.addChild(warlodSword);
+    inventario.addChild(wardlordSwordLabel);
+
+    inventoryScene.addChild(inventario);
   };
 
   game.onload = function () {
-    menuScene();
+    createMenuScene();
+    inventarioScene();
   };
 
   game.start();
