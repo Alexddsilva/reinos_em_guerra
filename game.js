b87bbe88d7e3c319ef7d042f297be5aabcc0650b
Leo B <leonardo_picinin02@hotmail.com>
2023-06-20 23:26:48 -0300

    menu scene

--- game.js
+++ game.js
@@ -11,14 +11,12 @@ window.onload = function () {
     "./images/characters/zyra.png",
   ]); // Preload de recursos, como imagens
 
-  const setCharacterMenu = () => {
+  const menuScene = () => {
     const menu = new Group(); // Grupo para conter as opções de personagens
     menu.x = 140; // Posição horizontal do menu
     menu.y = 150; // Posição vertical do menu
     menu.scaleX = 0.5;
     menu.scaleY = 0.5;
-
-    console.log(menu);
 
     const startX = 0;
     const imageSize = 512;
@@ -89,7 +87,7 @@ window.onload = function () {
   };
 
   game.onload = function () {
-    setCharacterMenu();
+    menuScene();
   };
 
   game.start();
