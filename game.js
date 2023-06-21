5c4396038adc35ca53c627040838ac183cd795c2
Leo B <leonardo_picinin02@hotmail.com>
2023-06-20 22:57:44 -0300

    controle do menu de personagens

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 enchant();
 
 window.onload = function () {
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
+  const screenWidth = 800;
+  const screenHeight = 600;
 
   const game = new Game(screenWidth, screenHeight);
   game.preload([
@@ -13,9 +13,8 @@ window.onload = function () {
 
   const setCharacterMenu = () => {
     const menu = new Group(); // Grupo para conter as opções de personagens
-    menu.x = screenWidth / 2; // Posição horizontal do menu
-    menu.y = screenHeight / 2; // Posição vertical do menu
-    menu.backgroundColor = "#ff0000";
+    menu.x = 140; // Posição horizontal do menu
+    menu.y = 150; // Posição vertical do menu
     menu.scaleX = 0.5;
     menu.scaleY = 0.5;
 
@@ -33,10 +32,15 @@ window.onload = function () {
     option1.scaleY = 0.5; // Redimensiona pela metade na vertical
     option1.x = startX;
     option1.y = 0;
+    option1.addEventListener("touchstart", () => {
+      alert("Escolheu Dargan");
+    });
 
-    const label1 = new Label("Personagem 1");
-    label1.x = 0;
-    label1.y = 520;
+    const label1 = new Label("Dargan");
+    label1.x = 200;
+    label1.y = 420;
+    label1.font = "30px Arial";
+    label1.color = "#FFF";
 
     menu.addChild(option1);
     menu.addChild(label1);
@@ -48,10 +52,15 @@ window.onload = function () {
     option2.scaleY = 0.5;
     option2.x = startX + imageHalfSize + spacing;
     option2.y = 0;
+    option2.addEventListener("touchstart", () => {
+      alert("Escolheu Elda");
+    });
 
-    const label2 = new Label("Personagem 2");
-    label2.x = 200;
-    label2.y = 520;
+    const label2 = new Label("Elda");
+    label2.x = 500;
+    label2.y = 420;
+    label2.font = "30px Arial";
+    label2.color = "#FFF";
 
     menu.addChild(option2);
     menu.addChild(label2);
@@ -63,10 +72,15 @@ window.onload = function () {
     option3.scaleY = 0.5;
     option3.x = startX + (imageHalfSize + spacing) * 2;
     option3.y = 0;
+    option3.addEventListener("touchstart", () => {
+      alert("Escolheu Zyra");
+    });
 
-    const label3 = new Label("Personagem 3");
-    label3.x = 400;
-    label3.y = 520;
+    const label3 = new Label("Zyra");
+    label3.x = 780;
+    label3.y = 420;
+    label3.font = "30px Arial";
+    label3.color = "#FFF";
 
     menu.addChild(option3);
     menu.addChild(label3);
